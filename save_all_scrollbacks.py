e450e705e369b09315be2a648e81bf0de75bae60
Yaakov Saxon <ysaxon@nabancard.com>
2021-06-17 13:40:02 -0400

    some changes, but still not working

--- save_all_scrollbacks.py
+++ save_all_scrollbacks.py
@@ -19,21 +19,30 @@ async def main(connection):
     print(app)
     for window in app.terminal_windows:
         print("window: "+window.pretty_str()+"\n")
+        pretty_window_safe=urllib.parse.quote(window.pretty_str().partition('\n')[0],safe='=[](){} ,+;:|?<>!@#$^&* ').replace(" ","_")
+        window_path=f"{path}/{pretty_window_safe}"
+        if not os.path.exists(window_path):
+            os.makedirs(window_path)
         for tab in window.tabs:
             for session in tab.sessions:
                 print(session.pretty_str())
                 pretty_session_safe=urllib.parse.quote(session.pretty_str(),safe='=[](){} ,+;:|?<>!@#$^&* ').replace(" ","_")
-                session_file_path=f"{path}/{pretty_session_safe}.txt"
+                session_file_path=f"{window_path}/{pretty_session_safe}.txt"
+                gzip_path=f"{window_path}/{pretty_session_safe}.txt.gz"
                 print(session_file_path)
+                lines_offset=0
+                lines_per_read=100000
                 async with iterm2.Transaction(connection) as txn:
-                  li = await session.async_get_line_info()
-                  lines = await session.async_get_contents(li.overflow, 100000000)
-                with open(session_file_path, "w") as f:
-                    f.writelines("{}\n".format(line.string) for line in lines)
+                    with open(session_file_path, "w") as f, gzip.open(gzip_path,"wt") as gf:
+                        try_reading_more_lines=True
+                        while try_reading_more_lines:
+                            li = await session.async_get_line_info()
+                            lines = await session.async_get_contents(li.overflow+lines_offset, lines_per_read)
+                            f.writelines("{}\n".format(line.string) for line in lines)        
+                            gf.writelines("{}\n".format(line.string) for line in lines)
+                            lines_offset+=lines_per_read
+                            try_reading_more_lines=lines
                 os.chmod(session_file_path, 0o200);
-                gzip_path=session_file_path+".gzip"
-                with gzip.open(gzip_path,"wt") as gf:
-                    gf.writelines("{}\n".format(line.string) for line in lines)
                 os.chmod(gzip_path, 0o200);
 
 iterm2.run_until_complete(main)
