12b435946b95377c57664588d6aa39d6fca669b4
Yaakov Saxon <ysaxon@nabancard.com>
2021-06-17 14:10:59 -0400

    commenting out the debugging print calls

--- save_all_scrollbacks.py
+++ save_all_scrollbacks.py
@@ -16,20 +16,20 @@ if not os.path.exists(path):
 
 async def main(connection):
     app = await iterm2.async_get_app(connection)
-    print(app)
+    #print(app)
     for window in app.terminal_windows:
-        print("window: "+window.pretty_str()+"\n")
+        #print("window: "+window.pretty_str()+"\n")
         pretty_window_safe=urllib.parse.quote(window.pretty_str().partition('\n')[0],safe='=[](){} ,+;:|?<>!@#$^&* ').replace(" ","_")
         window_path=f"{path}/{pretty_window_safe}"
         if not os.path.exists(window_path):
             os.makedirs(window_path)
         for tab in window.tabs:
             for session in tab.sessions:
-                print(session.pretty_str())
+                #print(session.pretty_str())
                 pretty_session_safe=urllib.parse.quote(session.pretty_str(),safe='=[](){} ,+;:|?<>!@#$^&* ').replace(" ","_")
                 session_file_path=f"{window_path}/{pretty_session_safe}.txt"
                 gzip_path=f"{window_path}/{pretty_session_safe}.txt.gz"
-                print(session_file_path)
+                #print(session_file_path)
                 lines_offset=0
                 lines_per_read=2000
                 async with iterm2.Transaction(connection) as txn:
@@ -37,10 +37,12 @@ async def main(connection):
                         try_reading_more_lines=True
                         while try_reading_more_lines:
                             li = await session.async_get_line_info()
+                            #print(f"overflow={li.overflow}")
                             lines = await session.async_get_contents(li.overflow+lines_offset, lines_per_read)
-                            f.writelines("{}\n".format(line.string) for line in lines)        
+                            f.writelines("{}\n".format(line.string) for line in lines)
                             gf.writelines("{}\n".format(line.string) for line in lines)
                             lines_offset+=lines_per_read
+                            #print(lines_offset)
                             try_reading_more_lines=lines
                 os.chmod(session_file_path, 0o200);
                 os.chmod(gzip_path, 0o200);
