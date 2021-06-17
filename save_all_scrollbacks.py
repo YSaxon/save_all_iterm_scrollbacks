b0b974d344bd8660d081b2c0113d48d4acff61c9
Yaakov Saxon <ysaxon@gmail.com>
2021-06-17 12:34:02 -0400

    got the script working

--- save_all_scrollbacks.py
+++ save_all_scrollbacks.py
@@ -1,43 +1,39 @@
 #!/usr/bin/env python3.7
 
 import iterm2
+import pprint
+import os
+import shlex
+import urllib
+import gzip
+
+
+path=os.path.expanduser("~")+'/Documents/iterm_screen_buffers'
+if not os.path.exists(path):
+    os.makedirs(path)
 # This script was created with the "basic" environment which does not support adding dependencies
 # with pip.
 
 async def main(connection):
-    # Your code goes here. Here's a bit of example code that adds a tab to the current window:
-    
-    print("line11")
-    #print(app.terminal_windows)
-    @iterm2.RPC
-    async def save_all_sessions():
-        app = await iterm2.async_get_app(connection)
-        print("beforeapp")
-        print(app)
-        print("afterapp")
-        print(app.terminal_windows)
-        print("lin13")
-        print(app.terminal_windows)
-        for window in app.terminal_windows:
-            print("lin15")
-            for tab in window.tabs:
-                for session in tab.sessions:
-                    async with iterm2.Transaction(connection) as txn:
-                      li = await session.async_get_line_info()
-                      print("got line info\n")
-                      lines = await session.async_get_contents(li.overflow, 10)
-                    print(list(map(lambda line: line.string, lines)))
-                      # lines = await session.async_get_contents(li.overflow, 10)
-  #                   print(list(map(lambda line: line.string, lines)))
-    
-    # async def clear_all_sessions():
-    #     code = b'\x1b' + b']1337;ClearScrollback' + b'\x07'
-    #     for window in app.terminal_windows:
-    #         for tab in window.tabs:
-    #             for session in tab.sessions:
-    #                 await session.async_inject(code)
-    print("lin32")
-    await save_all_sessions.async_register(connection)
-    print("lin34")
+    app = await iterm2.async_get_app(connection)
+    print(app)
+    for window in app.terminal_windows:
+        print("window: "+window.pretty_str()+"\n")
+        for tab in window.tabs:
+            for session in tab.sessions:
+                print(session.pretty_str())
+                pretty_session_safe=urllib.parse.quote(session.pretty_str(),safe='=[](){} ,+;:|?<>!@#$^&* ').replace(" ","_")
+                session_file_path=f"{path}/{pretty_session_safe}.txt"
+                print(session_file_path)
+                async with iterm2.Transaction(connection) as txn:
+                  li = await session.async_get_line_info()
+                  lines = await session.async_get_contents(li.overflow, 100000000)
+                with open(session_file_path, "w") as f:
+                    f.writelines("{}\n".format(line.string) for line in lines)
+                os.chmod(session_file_path, 0o200);
+                gzip_path=session_file_path+".gzip"
+                with gzip.open(gzip_path,"wt") as gf:
+                    gf.writelines("{}\n".format(line.string) for line in lines)
+                os.chmod(gzip_path, 0o200);
 
 iterm2.run_until_complete(main)
